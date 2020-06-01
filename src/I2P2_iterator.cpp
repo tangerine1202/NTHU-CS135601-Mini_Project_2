30845868ab4f9173267415bee03e9ebf2f1cfe06
Alan <alan0988469026@gmail.com>
2020-06-01 22:43:20 +0800

    feat: finish iterator

--- src/I2P2_iterator.cpp
+++ src/I2P2_iterator.cpp
@@ -20,7 +20,10 @@ namespace I2P2
   reference vector_iterator::operator[](difference_type offset) const {};
 
   // list_iterator
-  list_iterator::list_iterator(){};
+  list_iterator::list_iterator()
+  {
+    p = nullptr;
+  };
   iterator_impl_base &list_iterator::operator++(){};
   iterator_impl_base &list_iterator::operator--(){};
   iterator_impl_base &list_iterator::operator+=(difference_type offset){};
@@ -45,21 +48,78 @@ namespace I2P2
   bool const_iterator::operator>=(const const_iterator &rhs) const {};
 
   // iterator
-  iterator::iterator(){};
-  iterator::iterator(iterator_impl_base *p){};
-  iterator::iterator(const iterator &rhs){};
-  iterator &iterator::operator++(){};
-  iterator iterator::operator++(int){};
-  iterator &iterator::operator--(){};
-  iterator iterator::operator--(int){};
-  iterator &iterator::operator+=(difference_type offset){};
-  iterator iterator::operator+(difference_type offset) const {};
-  iterator &iterator::operator-=(difference_type offset){};
-  iterator iterator::operator-(difference_type offset) const {};
-  difference_type iterator::operator-(const iterator &rhs) const {};
-  pointer iterator::operator->() const {};
-  reference iterator::operator*() const {};
-  reference iterator::operator[](difference_type offset) const {};
+  iterator::iterator()
+  {
+    this->p_ = nullptr;
+  };
+  iterator::iterator(iterator_impl_base *p)
+  {
+    this->p_ = p;
+  };
+  iterator::iterator(const iterator &rhs)
+  {
+    this->p_ = rhs.p_;
+  };
+  iterator &iterator::operator++()
+  {
+    ++*(this->p_);
+    return *this;
+  };
+  iterator iterator::operator++(int)
+  {
+    iterator *ret = new iterator(*this);
+    ++*(this->p_);
+    return *ret;
+  };
+  iterator &iterator::operator--()
+  {
+    --*(this->p_);
+    return *this;
+  };
+  iterator iterator::operator--(int)
+  {
+    iterator *ret = new iterator(*this);
+    --*(this->p_);
+    return *this;
+  };
+  iterator &iterator::operator+=(difference_type offset)
+  {
+    *(this->p_) += offset;
+    return *this;
+  };
+  iterator iterator::operator+(difference_type offset) const
+  {
+    iterator *ret = new iterator(*this);
+    *(ret->p_) += offset;
+    return *ret;
+  };
+  iterator &iterator::operator-=(difference_type offset)
+  {
+    *(this->p_) -= offset;
+    return *this;
+  };
+  iterator iterator::operator-(difference_type offset) const
+  {
+    iterator *ret = new iterator(*this);
+    *(ret->p_) -= offset;
+    return *ret;
+  };
+  difference_type iterator::operator-(const iterator &rhs) const
+  {
+    return *(this->p_) - *rhs.p_;
+  };
+  pointer iterator::operator->() const
+  {
+    return &(this->operator*());
+  };
+  reference iterator::operator*() const
+  {
+    return *(*(this->p_));
+  };
+  reference iterator::operator[](difference_type offset) const
+  {
+    return *(*this + offset);
+  };
 
   /* Your definition for the iterator class goes here */
 } // namespace I2P2
